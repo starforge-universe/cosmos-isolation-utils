fdcee9af92fefd930e4a232604a79de2ceb0be99
Star Forge Worker <star.forge.worker@gmail.com>
2025-09-02 00:00:49 +0200

    fix runtime warning

--- cosmos_isolation_utils/__init__.py
+++ cosmos_isolation_utils/__init__.py
@@ -4,10 +4,9 @@ CosmosDB Isolation Utils
 A Python library providing utilities for Azure CosmosDB isolation and testing.
 """
 
-from .__main__ import main
-
 __version__ = "0.1.0"
 __author__ = "Starforge Worker"
 __email__ = "star.forge.worker@gmail.com"
 
-__all__ = ['main']
+# Note: main function is available when running as module
+# but not imported here to avoid circular import issues
